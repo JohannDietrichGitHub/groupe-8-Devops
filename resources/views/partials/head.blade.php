160894d2164f00f241d3250b52b0671d6c3d1316
Johann Github <83764436+JohannDietrichGitHub@users.noreply.github.com>
2024-06-26 14:29:47 +0200

    Ajout de pages pour créer une structure basique au projet

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'Votre titre par défaut' }}</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/result.css') }}" rel="stylesheet">
+</head>
