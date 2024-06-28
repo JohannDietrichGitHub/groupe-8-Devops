f33f3e26da1a95a4817f91593d98f07ccdad36a2
Johann Github <83764436+JohannDietrichGitHub@users.noreply.github.com>
2024-06-28 10:36:25 +0200

    Ajout du formulaire fonctionnel et du style de celui-ci

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,5 +3,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Votre titre par défaut' }}</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ asset('css/result.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/results.css') }}" rel="stylesheet">
 </head>
